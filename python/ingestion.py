57d7d967cf277e6cd93435ca175fd736798d0f7c
AnyaSaxena24 <anya.saxena24@gmail.com>
2026-04-20 03:40:47 +0530

    Fixed transformation and ingestion scripts

--- python/ingestion.py
+++ python/ingestion.py
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
 from transformation import load_data, clean_data, transform_data, validate_data, save_data
 
+# Step 1: Load data
 df = load_data('../data/sales_data.csv')
 
+# Step 2: Clean data
 df = clean_data(df)
 
+# Step 3: Transform data
 df = transform_data(df)
 
+# Step 4: Validate data
 df = validate_data(df)
 
-=======
-from transformation import load_data, clean_data, transform_data, validate_data, save_data
-
-df = load_data('../data/sales_data.csv')
-
-df = clean_data(df)
-
-df = transform_data(df)
-
-df = validate_data(df)
-
->>>>>>> 0ba55a4fda3736f5faf58da868efe97116708868
+# Step 5: Save processed data
 save_data(df, '../output/processed_sales_data.csv')
+
+print("Pipeline executed successfully!")
